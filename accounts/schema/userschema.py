cbf4a48d785a63a742311ea5b894f9c082dde204
Mukilan <mukilan007k@gmail.com>
2022-03-13 15:46:16 +0530

    login comparation with DB

--- accounts/schema/userschema.py
+++ accounts/schema/userschema.py
@@ -5,10 +5,15 @@ class Register:
     def __init__(self):
         self.data = {
             "first_name": request.form["first_name"],
-            # "last_name": request.form["last_name"],
+            "last_name": request.form["last_name"],
             "email": request.form["E-mail"],
             "password": request.form["password"],
-            # "contact_no": request.form["contact_no"]
+            "contact_no": request.form["contact_no"],
+            # "created_by": created_by(),
+            # "created_at": created_at(),
+            # "modified_by": modified_by(),
+            # "modified_at": modified_at(),
+            "is_deleted": False
         }
 
     def sign_up(self):
--- accounts/services/accounts_services.py
+++ accounts/services/accounts_services.py
@@ -5,16 +5,27 @@ from metadata.main import db_account
 
 class AccountService():
     """
-    This service is used to maintain the account of the user
+            This service is used to maintain the account of the user
     """
 
-    def  signin(self):
+    def signin(self):
+        """
+                redirecting to user signin page
+        """
         return render_template("login.html")
 
     def create_account(self):
+        """
+                redirecting to new user signup page
+        """
         return render_template("register.html")
 
     def db_register(self):
+        """
+                getting information from signup page,
+                comparing to DB collection and creating new account
+        :return: redirecting to new user home page
+        """
         respond = Register().sign_up()
         if db_account.UserMetadata.find_one({"email": respond['email']}):
             return jsonify({"error": "Email address already exist"}), 400
@@ -25,10 +36,19 @@ class AccountService():
     def login_view(self):
         user_email = request.form['Email']
         user_password = request.form['Password']
-        if user_email == "mukilan007k@gmail.com":
-            session['user_email'] = user_email
+        db_email = list(db_account.UserMetadata.find({"email": f"{user_email}"}))
+        value = None
+        db_password = None
+        for i in db_email:
+            value = i["email"]
+            # db_password = fernet.decrypt(i["password"]).decode()
+            db_password = i['password']
+        if user_email == value and user_password == db_password:
+            # session['user'] = db_email
+            # for i in db_email:
+            #     session['id'] = i["_id"]
             return render_template("homepage.html")
-        return f"<h1> {user_email} </h1>"
+        return "Incorrect email id or password, Try Again... "
 
     # if len(user)>0:
 
